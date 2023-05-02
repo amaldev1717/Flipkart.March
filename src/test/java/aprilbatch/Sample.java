80743c593deabb463377723611e0e00874e53fca
unknown <ronaldo.dev20@gmail.com>
2023-05-02 14:08:25 +0530

    message

---
+++ src/test/java/aprilbatch/Sample.java
@@ -0,0 +1,79 @@
+package aprilbatch;
+
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.sql.Driver;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.dom4j.util.UserDataAttribute;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByTagName;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Sample {
+
+	public static void main(String[] args) throws Throwable {
+		WebDriverManager.edgedriver().setup();
+
+		WebDriver driver = new EdgeDriver();
+
+		driver.get("https://demo.guru99.com/test/web-table-element.php");
+	
+		int rowSize = driver.findElements(By.xpath("//table[@class='dataTable']//tbody//tr")).size();
+        System.out.println(rowSize);
+        
+        int coloumSize = driver.findElements(By.xpath("//table[@class='dataTable']//tbody//tr[2]//td")).size();
+        System.out.println(coloumSize);
+        
+        for (int i = 1; i <=rowSize ; i++) {
+        	for (int j = 1; j <=coloumSize; j++) {
+        		WebElement element = driver.findElement(By.xpath("//table[@class='dataTable']//tbody//tr["+i+"]//td["+j+"]"));
+				String content=element.getText();
+//				
+			
+        	if(i==25) {
+        		
+				System.out.println(content);
+        	}
+        	}
+			
+		}
+        
+			
+	}
+
+}
+
+
+
+
